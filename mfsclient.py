df1f045dd4caa4de2f764464c3d919e05dd15393
Mathias Ruediger <ruediger@blueboot.org>
2011-06-16 14:54:30 +0200

    Basic client skeleton

---
+++ mfsclient.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+    
+import sys,os
+import mfs
+
+def export(argv):
+    try:
+        manifest_path  = argv[2]
+        datastore_path = argv[3]
+        destination_path = argv[4]
+    except IndexError as e:
+        print e
+        return
+
+    for dir in (destination, datastore):
+        if not (os.path.exists(dir) and os.path.isdir(dir)):
+            print ("ERROR: %s not a direfctory" % dir)
+            return
+
+    manifest = mfs.manifest.manifestFromPath(
+        manifest_path,
+        mfs.datastore.Datastore(datastore))
+
+    for file in manifest:
+        file.writeTo(destination)
+    
+    
+def print_help(argv):
+    pass
+
+
+if __name__ == "__main__":
+    {
+        'export' : export
+    }.get(sys.argv[1], print_help)(sys.argv)
