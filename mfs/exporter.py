3d0ddf13a75f0ee2f0a4cbca8589941f5201db74
Mathias Ruediger <ruediger@blueboot.org>
2011-07-01 14:29:12 +0000

    further exporter testing

--- mfs/exporter.py
+++ mfs/exporter.py
@@ -16,28 +16,33 @@ class Exporter(object):
             pass
 
         if isinstance(node, mfs.manifest.SymbolicLink):
-            pass
+            os.symlink(target + '/' + node.target, path)
 
         if isinstance(node, mfs.manifest.Device):
-            pass
+            os.makedev(
+                os.major(node.st_rdev),
+                os.minor(node.st_rdev)
+                )
 
         if isinstance(node, mfs.manifest.FIFO):
-            pass
+            os.mkfifo(path)
 
         if isinstance(node, mfs.manifest.Directory):
-            pass
+            os.mkdir(path, mode=node.st_mode)
 
         if isinstance(node, mfs.manifest.File):
             if (node.st_nlink > 1):
                 if (node.hash in self.linkcache):
                     os.link(self.linkcache[node.hash], path)
+                    return
                 else:
                     self.linkcache[node.hash] = path
-                    source = datastore.getData(node)
-                    dest = file(target, 'w')
-                    for data in source.read(1024):
-                        dest.write(data)
-                    dest.flush()
+
+            source = datastore.getData(node)
+            dest = file(path, 'w')
+            for data in source.read(1024):
+                dest.write(data)
+            dest.flush()
 
         
         os.chown(path, node.st_uid, node.st_gid)        #TODO security
--- tests/export-test.py
+++ tests/export-test.py
@@ -29,11 +29,35 @@ class TestExport(unittest.TestCase):
         exporter = Exporter()
         exporter.export(directory, self.tmpdir, None)
         self.assertTrue(os.path.isdir(self.tmpdir + '/' + "test"))
-
         self.assertEquals(1234567, os.stat(self.tmpdir + '/' + "test")[ST_ATIME])
         self.assertEquals(12345678, os.stat(self.tmpdir + '/' + "test")[ST_MTIME])
-
         self.assertEquals(16866, os.stat(self.tmpdir + '/' + "test")[ST_MODE])
+
+    def testExportFile(self):
+        testfile = File("file")
+        testfile.st_uid = 1000
+        testfile.st_gid = 1000
+        testfile.st_atime = 1234567
+        testfile.st_mtime = 7654321
+        testfile.st_nlink = 0
+        testfile.st_mode  = 33188
+
+        (num, tmpfile) = tempfile.mkstemp()
+        node = File(tmpfile)
+        file(tmpfile, 'w').write("testcontent")
+        datastore = Datastore()
+        datastore.saveData(node, tmpfile)
+        os.remove(tmpfile)
+
+        testfile.hash = node.hash
+
+        exporter = Exporter()
+        exporter.export(testfile, self.tmpdir, datastore)
+        self.assertTrue(os.path.exists(self.tmpdir + '/' + 'file'))
+        self.assertEquals(1234567, os.stat(self.tmpdir + '/' + 'file')[ST_ATIME])
+        self.assertEquals(7654321, os.stat(self.tmpdir + '/' + 'file')[ST_MTIME])
+        self.assertEquals(33188, os.stat(self.tmpdir + '/' + 'file')[ST_MODE])
+
 
 if __name__ == '__main__':
     unittest.main()
