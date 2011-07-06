4dec31ae708790cfc32051216b22669eee327225
Mathias Ruediger <ruediger@blueboot.org>
2011-07-06 14:34:20 +0200

    improved command line interface

--- mfsclient.py
+++ mfsclient.py
@@ -5,11 +5,11 @@ import mfs
 
 def export(argv):
     try:
-        manifest_file  = argv[2]
-        datastore_path = argv[3]
-        destination_path = argv[4]
+        manifest_file  = argv[0]
+        datastore_path = argv[1]
+        destination_path = argv[2]
     except IndexError as e:
-        print e
+        print "usage: {0} export MANIFEST DATASTORE DESTINATION".format(sys.argv[0])
         return
 
     for dir in (destination_path, datastore_path):
@@ -27,12 +27,31 @@ def export(argv):
     for child in manifest.root.children:
         exporter.export(child, destination_path, datastore)
     
-    
-def print_help(argv):
-    pass
+def create(argv):
+    try:
+        source_path  = argv[0]
+        datastore_path = argv[1]
+    except IndexError:
+        print "usage: {0} create PATH DATASTORE".format(sys.argv[0])
+        return
 
+    manifest = mfs.manifestFromPath(
+        source_path,
+        mfs.datastore.Datastore(datastore))
+
+    print etree.tostring(manifest.toXML(), pretty_print=True)
 
 if __name__ == "__main__":
-    {
-        'export' : export
-    }.get(sys.argv[1], print_help)(sys.argv)
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="MFS Client"
+    )
+    parser.add_argument('ACTION', type=str, help="action to take", choices=["export", "merge", "mount"]
+    )
+    parser.add_argument('OPTION', type=str, nargs="*")
+    args = parser.parse_args()
+    
+    if (args.ACTION == "export"):
+        export(args.OPTION)
+    else:
+        parser.print_help()
