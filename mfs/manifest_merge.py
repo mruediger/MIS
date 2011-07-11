a46373f71d22ae5ebe6c3d7037a9da70a95dc02c
Mathias Ruediger <ruediger@blueboot.org>
2011-07-11 18:36:04 +0000

    added delnode handling

--- mfs/manifest_merge.py
+++ mfs/manifest_merge.py
@@ -1,21 +1,16 @@
-from mfs.manifest import Directory, Manifest
+from mfs.manifest import Directory, Manifest, DeleteNode
 from threading import Thread
 
 import time
 
-def unionmerge(target, unionfsdir):
-    for child in unionfsdir.children:
-        if child.name.endswith('_HIDDEN~'):
-            for checkdel in target.children:
-                if checkdel.name == child.name.rstrip('_HIDDEN~'):
-                    target.children.remove(checkdel)
-        else:
-            unionmerge(target.children_as_dict[child.name], child)
-
 def merge_children(orig, new):
-    filenames = set()
+    regnodes = set()
+    delnodes = set()
     for child in orig.children + new.children:
-        filenames.add(child.name)
+        if isinstance(child, DeleteNode):
+            delnodes.add(child)
+        else:
+            regnodes.add(child.name)
 
     odict = orig.children_as_dict
     ndict = new.children_as_dict
@@ -24,13 +19,6 @@ def merge_children(orig, new):
     retval = list()
 
     for filename in filenames:
-                
-        #filter AUFS files
-        if ('.wh.' + filename in ndict):
-            continue
-        if ('.wh.' + filename in odict):
-            continue
-
         if (filename in ndict):
             newchild = ndict[filename].copy()
         else:
@@ -44,18 +32,12 @@ def merge_children(orig, new):
         retval.append(newchild)
         
 
-    return retval
+    return retval + list(delnodes)
         
 
 def merge(orig, new):
     target = new.root.copy()
     target.children = merge_children(orig.root, new.root)
-
-    #check for unionfs
-    for child in target.children:
-        if (child.name == '.unionfs'):
-            unionmerge(target, child)
-            target.children.remove(child)
 
     return Manifest(target)
 
