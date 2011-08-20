2fe7655d5e242fd0ef2d9da81c4f0fa6fd036040
Mathias Ruediger <ruediger@blueboot.org>
2011-08-20 18:38:58 +0000

    basic user interface added

--- mfsserver.py
+++ mfsserver.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
 
+import sys
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer as Server
 
 from mfs.repository import FileRepository
 from mfs.server import MFSServer
 
-def run():
-    repository = FileRepository('tmp/repository')
+def run(repo, port, autopatch):
+    repository = FileRepository(repo)
 
-    server = Server(('', 4321))
-    server.register_instance(MFSServer(repository))
+    server = Server(('', int(port)))
+    server.register_instance(MFSServer(repository, None))
     server.serve_forever()
 
 if __name__ == '__main__':
-    run()
+    try:
+        repo = sys.argv[1]
+        port = sys.argv[2]
+        if len(sys.argv) > 3:
+            filterscript = sys.argv[3]
+            run(repo, port, autopatch)
+        else:
+            run(repo, port, None)
+    except IndexError:
+        print "usage {0} repository port".format(sys.argv[0])
