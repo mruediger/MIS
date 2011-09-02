577adad5a7ab8e1d4474e2cb47342bb34de85757
Mathias Ruediger <ruediger@blueboot.org>
2011-09-02 10:21:45 +0000

    statfs calls added

--- mfs/fs.py
+++ mfs/fs.py
@@ -23,6 +23,25 @@ class Operations(llfuse.Operations):
         self.filecache = dict()
         self.hardlinks = dict()
 
+
+        self.fstat = llfuse.StatvfsData()
+        #optimal transfer block size
+        self.fstat.f_bsize = int(self.manifest.root.stats.st_blksize)
+        #total data blocks in file system
+        self.fstat.f_blocks = 0
+        #free blocks in fs
+        self.fstat.f_bfree = 0
+        #free blocks available to unprivileged user
+        self.fstat.f_bavail = 0
+        #total file nodes in file system
+        self.fstat.f_files = 0
+        #free file nodes in fs
+        self.fstat.f_ffree = 0
+        self.fstat.f_favail = 0
+        
+        #fragment size
+        self.fstat.f_frsize = int(self.manifest.root.stats.st_blksize)
+
         self.highest_inode = 1
         self.genEntryAndInode(manifest.root)
 
@@ -54,6 +73,9 @@ class Operations(llfuse.Operations):
 
         node.inode = self.highest_inode
         self.highest_inode += 1
+
+        self.fstat.f_blocks += int(entry.st_blocks)
+        self.fstat.f_files += 1
 
         self.entrycache.append( entry )
         self.nodecache.append( node )
@@ -98,3 +120,10 @@ class Operations(llfuse.Operations):
 
     def readlink(self, inode):
         return self.nodecache[inode].target
+
+    def statfs(self):
+        """Get file system statistics"""
+        return self.fstat
+
+
+
